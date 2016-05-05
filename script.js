f140c573905836c148ee53cf234b0f0308d3d813
Awaghma <Arvind.Waghmare@morningstar.com>
2016-05-05 22:06:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,119 @@
+// Code goes here
+
+var app = angular.module("app",[]);
+
+app.controller("MainController",["$scope",function($scope){
+    $scope.title = "Timeline";
+    $scope.chatHistory = [
+        {
+            "incidentId": 100,
+            "eventId": 95840662519808,
+            "eventSource": "SkypeBot",
+            "receivedTime": "2016-05-05T01:40:53.3701631+00:00",
+            "occuredTime": "2016-05-04T18:40:53.0815123-07:00",
+            "message": {
+                "title": "Books panel AGP connect killer Incredible Cotton Sausages Won Practical Cotton Car transform tan ADP Executive",
+                "text": "Peter_White9 joined chat Trinidad and Tobago"
+            }
+        },
+        {
+            "incidentId": 100,
+            "eventId": 95840662585344,
+            "eventSource": "ACIS",
+            "receivedTime": "2016-05-05T01:40:54.1712079+00:00",
+            "occuredTime": "2016-05-04T18:40:53.8835156-07:00",
+            "message": {
+                "title": "applications enable forecast Generic Granite Fish pink Dynamic expedite",
+                "text": "In rerum consequuntur necessitatibus facilis reiciendis ratione. Molestiae soluta sed et. Fuga qui autem voluptas nobis quos et facilis exercitationem. Id quaerat et pariatur itaque. Corporis autem blanditiis porro. Labore nesciunt aut."
+            }
+        },
+        {
+            "incidentId": 100,
+            "eventId": 95840662585345,
+            "eventSource": "IcM",
+            "receivedTime": "2016-05-05T01:40:54.2960726+00:00",
+            "occuredTime": "2016-05-04T18:40:54.0196783-07:00",
+            "message": {
+                "title": "Innovative Automotive Avon Factors Ville South Carolina Swiss Franc Factors Awesome Netherlands",
+                "text": "Incident №1 resolved by Brannon12"
+            }
+        },
+        {
+            "incidentId": 100,
+            "eventId": 95840662585346,
+            "eventSource": "MDM",
+            "receivedTime": "2016-05-05T01:40:54.421086+00:00",
+            "occuredTime": "2016-05-04T18:40:54.1453751-07:00",
+            "message": {
+                "title": "Computers & Baby withdrawal stable Granite Illinois Avon Bahrain global Computers, Shoes & Jewelery applications New Hampshire AI Pennsylvania",
+                "text": "Dolore quia officia hic et deserunt vitae tenetur numquam. Vel dolorem deleniti iste sed consequatur autem reiciendis accusamus id. Sed ut illum pariatur illum fugit fugiat iure odio natus."
+            }
+        },
+        {
+            "incidentId": 100,
+            "eventId": 95840662585347,
+            "eventSource": "SkypeBot",
+            "receivedTime": "2016-05-05T01:40:54.4680197+00:00",
+            "occuredTime": "2016-05-04T18:40:54.1721134-07:00",
+            "message": {
+                "title": "grow User-friendly Direct fuchsia Factors Industrial, Computers & Garden Movies & Kids Trail Trail CFA Franc BCEAO bypass homogeneous local",
+                "text": "Omari77 leaved chat cross-media"
+            }
+        },
+        {
+            "incidentId": 100,
+            "eventId": 95840662650880,
+            "eventSource": "SkypeBot",
+            "receivedTime": "2016-05-05T01:40:55.5510766+00:00",
+            "occuredTime": "2016-05-04T18:40:55.2692416-07:00",
+            "message": {
+                "title": "Steel green Handmade Granite Bacon Connecticut feed Awesome Web Handcrafted Wooden Towels Buckinghamshire",
+                "text": "Lillian.Feil46 joined chat reinvent"
+            }
+        },
+        {
+            "incidentId": 100,
+            "eventId": 95840662716416,
+            "eventSource": "ACIS",
+            "receivedTime": "2016-05-05T01:40:56.2210532+00:00",
+            "occuredTime": "2016-05-04T18:40:55.9394278-07:00",
+            "message": {
+                "title": "dedicated Sleek Steel Soap Research Avon back-end Lead Direct connecting Greece",
+                "text": "Et id hic maiores modi. Et et optio quidem magni consequuntur ullam. Aut enim eum eveniet sint ea soluta eum. In ad rerum."
+            }
+        },
+        {
+            "incidentId": 100,
+            "eventId": 95840662716417,
+            "eventSource": "IcM",
+            "receivedTime": "2016-05-05T01:40:56.3641967+00:00",
+            "occuredTime": "2016-05-04T18:40:56.0867592-07:00",
+            "message": {
+                "title": "Buckinghamshire Monitored Netherlands Antillian Guilder District Oval Fantastic Cotton Ball e-services Yen synthesize white Route Land Grocery, Toys & Toys Cedi withdrawal",
+                "text": "Incident №0 resolved by Jamal.Dickens"
+            }
+        },
+        {
+            "incidentId": 100,
+            "eventId": 95840662716418,
+            "eventSource": "MDM",
+            "receivedTime": "2016-05-05T01:40:56.4892367+00:00",
+            "occuredTime": "2016-05-04T18:40:56.2216358-07:00",
+            "message": {
+                "title": "Locks compressing Wooden backing up programming index mesh index Soft payment Creek Licensed Metal Computer",
+                "text": "Repellendus officia ipsum qui. Ratione atque laborum quae est. Non molestiae molestias vel quia. Facere eligendi in sunt. A aliquid similique itaque aliquam sit velit accusantium similique. Quaerat distinctio aliquam."
+            }
+        },
+        {
+            "incidentId": 100,
+            "eventId": 95840662716419,
+            "eventSource": "SkypeBot",
+            "receivedTime": "2016-05-05T01:40:56.6298708+00:00",
+            "occuredTime": "2016-05-04T18:40:56.3562924-07:00",
+            "message": {
+                "title": "protocol Intelligent Steel Keyboard sexy Berkshire lavender capacitor invoice",
+                "text": "Gwen.Lang joined chat Toys & Toys"
+            }
+        }
+    ]
+}]);
